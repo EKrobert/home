57e00a3b4027f25fb1b8401f5e82e54c6a0b53bc
Robert EZIAN <E.K.Robert>
2023-09-28 17:03:58 +0000

    first angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  
+    providers: [],
+    imports: [
+        BrowserModule,
+        HomeComponent
+    ]
 })
 export class AppModule { }
